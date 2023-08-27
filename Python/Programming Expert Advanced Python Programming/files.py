ba011bfe4d481112466ca9274e5a402b9c3376a4
Wanderer <backfrontdev47@gmail.com>
2023-08-27 17:17:51 +0600

    file modes

--- Python/Programming Expert Advanced Python Programming/files.py
+++ Python/Programming Expert Advanced Python Programming/files.py
@@ -11,3 +11,12 @@ Python Files
 
 # - Append mode appends at the end of an existing file
 # - In append mode we are not overwriting the file we are just appending new data at the end of the file
+
+#- r+ mode is for reading file and writing. We can simply read and write in a file with this mode.
+
+
+# - file.seek() Changes the cursor location in the file
+
+# - when we do file.read(), the cursor's position is at the end of the file.
+# - Because file.read() reads the whole file, so cursor stays at the end of the file.
+# - But we can change cursor position manually with file.seek() method
