09a3a8a495405491fa84e80858860a1a28200b9c
Wanderer <backfrontdev47@gmail.com>
2023-08-27 17:09:34 +0600

    file modes

---
+++ Python/Programming Expert Advanced Python Programming/files.py
@@ -0,0 +1,13 @@
+'''
+Python Files
+'''
+
+# - When we use file.read() it will give us all the text inside the file
+# - When we use file.readlines() it will give us a list of all the lines in the file
+# - We can use replace method to remove white space from the middle of a sentence
+
+# - When we are using write mode we have to be careful, Because write mode not only creates new files
+# - It also override the same existing file
+
+# - Append mode appends at the end of an existing file
+# - In append mode we are not overwriting the file we are just appending new data at the end of the file
