eb05e779fa8e96d5894efc76c0c64d0506d6aded
Wanderer <backfrontdev47@gmail.com>
2023-08-27 16:48:05 +0600

    Modules and packages

---
+++ Python/Programming Expert Advanced Python Programming/modules and packages.py
@@ -0,0 +1,11 @@
+"""
+Module and Packages help us to better organize our python code.
+A Python module is just a python file.
+So anything that ends with .py file extension is a python module.
+And a Python Package is simply a directory / folder containing Python modules.
+So this allows us to better separate our code and organize it.
+"""
+
+
+# - When we execute a python file in the terminal it works as the main module
+# - __init__.py makes a folder a python package
