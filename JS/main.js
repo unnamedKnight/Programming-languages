742f92106d4de482aa995791649bb68c7288a25d
Wanderer <backfrontdev47@gmail.com>
2023-09-29 19:43:43 +0600

    Finished conditionals

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,1 @@
-// if we want to add a `$` sign in front of variables
-// then we have to do the following
-// `You bought ${quantity} ${items}, total price: $${price * quantity}`;
-// Here, the first `$` sign works as a string and second `$` sign works
-// as the part of the String literate template 
-
-
-alert('its working.');
-console.log('hello world');
+console.log("we are good");
