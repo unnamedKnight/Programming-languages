dc14709e1ec8a665fd05dfac1fefbfe670f65e77
Wanderer <backfrontdev47@gmail.com>
2023-09-29 11:18:00 +0600

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+// if we want to add a `$` sign in front of variables
+// then we have to do the following
+// `You bought ${quantity} ${items}, total price: $${price * quantity}`;
+// Here, the first `$` sign works as a string and second `$` sign works
+// as the part of the String literate template 
+
+
+alert('its working.');
