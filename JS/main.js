860c8a03411ea737440e0cadf43b9db835bebb3c
unnamedKnight <backfrontdev47@gmail.com>
2023-09-30 20:45:31 +0600

    For loops in JS

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,20 @@
 console.log("we are good");
 
+/* ------------------------- An important note here ------------------------- */
+/* 
+for loop works like following
+for (let index = 0; index < 10) {
+    console.log(`HELLO!!! ${index}`)
+    index++
+}
+which means it will increment or decrement after it runs
+whatever is within the {} braces of for loop
+
+
+*/
+
+
+for (let index = 0; index < 10; index++) {
+    console.log(`HELLO!!! ${index}`)
+}
+
