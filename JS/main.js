e52cd1ac56266261c1d3be71c1ff4c407a5da3f0
Wanderer <backfrontdev47@gmail.com>
2023-10-03 13:49:11 +0600

    Scope in js Done

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,3 @@
 console.log("we are good");
 
-function rollYourDice() {
-    randomNumber = 0
-    while (randomNumber === 0){
-        randomNumber= Math.floor(Math.random() * 7)
-    }
-    return randomNumber
-}
 
-console.log(rollYourDice())
