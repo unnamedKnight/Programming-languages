7ecf15f7b5224cdc78b12f61ea5b2df145498c08
unnamedKnight <backfrontdev47@gmail.com>
2023-10-10 23:16:52 +0600

    Implement deck of card Within an Object

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,43 @@
-console.log('We are good !!!')
+console.log("We are good !!!");
 
+const deckOfCards = {
+  deck: [],
+  drawnCards: [],
+  suits: ["heart", "spades", "clubs", "diamonds"],
+  values: "2,3,4,5,6,7,8,9,10,J,Q,K,A",
+  initializeDeck() {
+    // destructing deckOfCards object with this keyword
+    // here, this keyword refers to the scope of deckOfCards object
+    const { deck, suits, values } = this;
+    for (let suit of suits) {
+      for (let value of values.split(",")) {
+        deck.push({
+          suit,
+          value,
+        });
+      }
+    }
+  },
+
+  drawSingleCard() {
+    const card = this.deck.pop();
+    this.drawnCards.push(card);
+    return card;
+  },
+
+  drawMultipleCards(target) {
+    const arr = [];
+    for (let start = 0; start < target; start++) {
+      arr.push(this.drawSingleCard());
+    }
+    return arr;
+  },
+
+  shuffleDeck() {
+    const deck = this.deck;
+    for (let index = deck.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * deck.length);
+      [deck[index], deck[randomIndex]] = [deck[randomIndex], deck[index]];
+    }
+  },
+};
