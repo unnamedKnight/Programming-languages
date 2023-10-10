5a7903cc6909790c31f8c8afff917937d20e6c7a
Wanderer <backfrontdev47@gmail.com>
2023-10-10 10:23:32 +0600

    Object methods and this keyword

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,29 @@
-console.log('We are good !!!')
+console.log("We are good !!!");
 
+const getStats = (arr) => {
+  const max = Math.max(...arr);
+  const min = Math.min(...arr);
+  const sum = arr.reduce((sum, r) => sum + r);
+  const avg = sum / arr.length;
+  // If we return variables within object literals from a function()
+  // we don't need to specify the key name separately
+  // we can simply return the variables within object literal
+  // the variable name will be the key and the value of that
+  // variable will be assigned to that key
+  // Using the new shorthand property syntax:
+  return {
+    max,
+    min,
+    sum,
+    avg,
+  };
+};
+const reviews = [4.5, 5.0, 3.44, 2.8, 3.5, 4.0, 3.5];
 
+const stats = getStats(reviews);
 
+function pick(arr) {
+  //return random element from arr
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
