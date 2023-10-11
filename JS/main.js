48fd097239150e429a59d8ed05943523cfb07b38
Wanderer <backfrontdev47@gmail.com>
2023-10-11 08:48:45 +0600

    Add some global notes some global note

--- JS/main.js
+++ JS/main.js
@@ -41,3 +41,22 @@ const deckOfCards = {
     }
   },
 };
+/* -------------------------------------------------------------------------- */
+/*                                 GLOBAL NOTE                                */
+/* -------------------------------------------------------------------------- */
+// window is the global scope on the browser
+// when we define a function like following
+// function sayHi() {
+//   console.log("HI");
+// }
+// this `sayHi` function is added as property in the `window object`
+// In short any function normally refers to the `window object` in browser
+// `var` declarations are added to the window object but `let` and `const` aren't
+
+/* -------------------------------------------------------------------------- */
+/*                                     END                                    */
+/* -------------------------------------------------------------------------- */
+
+
+
+
